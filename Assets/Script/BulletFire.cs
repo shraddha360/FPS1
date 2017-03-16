2d3503008e9edc6f88819fa21aa4bb1f0262932b
Sharddha Shrikant Pote <shraddha.p@vmokshagroup.com>
2017-03-16 13:26:12 +0530

    FPS1 - C# scripting L1

---
+++ Assets/Script/BulletFire.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BulletFire : MonoBehaviour
+{
+	
+	public GameObject Bullet_Emitter;
+
+
+	public GameObject Bullet;
+
+	public float Bullet_Forward_Force;
+
+	// Use this for initialization
+	void Start ()
+	{
+
+	}
+
+	// Update is called once per frame
+	void Update ()
+	{
+		if (Input.GetKeyDown (KeyCode.F)) {
+			GameObject Temporary_Bullet_Handler;
+			Temporary_Bullet_Handler = Instantiate (Bullet, transform.position, transform.rotation) as GameObject;
+
+	
+			Temporary_Bullet_Handler.transform.Rotate (Vector3.left * Bullet_Forward_Force );
+
+			Rigidbody Temporary_RigidBody;
+			Temporary_RigidBody = Temporary_Bullet_Handler.GetComponent<Rigidbody> ();
+
+			Temporary_RigidBody.AddForce (transform.right  * Bullet_Forward_Force);
+
+
+			Destroy (Temporary_Bullet_Handler, 2.0f);
+
+		
+		}
+
+
+	}
+
+}
---
+++ Assets/Script/EnemyDestroy.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+
+
+public class EnemyDestroy : MonoBehaviour {
+	
+	public int scoreValue = 10;
+
+
+
+	void OnCollisionEnter(Collision collision){
+
+
+		if(collision.gameObject.tag == "Bullet")
+		{
+			Debug.Log ("Destroy");
+			//Destroy (collision.gameObject);
+
+			ScoreText.score += scoreValue;
+
+			Destroy(this.gameObject);
+
+
+
+		}
+	}
+
+}
